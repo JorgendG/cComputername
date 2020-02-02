e2e4c80c10b2668c73bed6bc64028e0b9b0a9297
Jorgen de Gier <jorgen.de.gier@salomon-it.nl>
2020-02-02 22:40:13 +0100

    init commit

---
+++ cComputerName.psm1
@@ -0,0 +1,62 @@
+enum Ensure
+{
+  Present
+  Absent
+}
+
+[DscResource()]  class cComputerName ##  MyFolder is the name of the resource
+{
+    [DscProperty(Mandatory,Key)]
+    [Ensure]$Ensure
+
+    ## What to do if it's  not in the right state. This returns nothing, indicated by [void].
+
+    [void] Set() 
+    {
+        if ($this.Ensure -eq  [Ensure]::Present)
+        {
+            $regvalue = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" -Name "VirtualMachineName" -ErrorAction SilentlyContinue
+
+            Rename-Computer -NewName $regvalue.VirtualMachineName -Restart
+        }
+        elseif ($this.Ensure  -eq [Ensure]::Absent)
+        {
+            # eh, doe iets
+        }
+    }
+
+  ## Test to ensure  it's in the right state. This returns a Boolean value, indicated by [bool].
+
+  [bool] Test() 
+    {
+        #HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters
+        $regvalue = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" -Name "VirtualMachineName" -ErrorAction SilentlyContinue
+        $rootfolderset = $regvalue.VirtualMachineName -eq $env:COMPUTERNAME
+        if ($this.Ensure -eq  [Ensure]::Present)
+        {
+            return $rootfolderset
+        }
+        else
+        {
+            return -not  $rootfolderset
+        }
+    }
+
+  ## Get the state.  This returns an instance of the class itself, indicated by [MyFolder]
+
+    [cComputerName] Get() 
+    {
+        #
+        $regvalue = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" -Name "VirtualMachineName" -ErrorAction SilentlyContinue
+        if( $regvalue.RootFolder -eq $env:COMPUTERNAME )
+        {
+            $this.Ensure = [Ensure]::Present
+        }
+        else 
+        {
+            $this.Ensure = [Ensure]::Absent
+        }
+        return $this
+    }
+}
+
